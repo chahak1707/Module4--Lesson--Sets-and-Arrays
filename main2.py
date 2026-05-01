7086f2d45737c338a594bf58075e8bb5bc021913
chahak1707 <pahujamanni1978@gmail.com>
2026-05-01 22:26:50 +0530

    module4-lesson-sets and arrays

---
+++ main2.py
@@ -0,0 +1,8 @@
+set_x= {"green","blue"}
+set_y= {"blue","yellow"}
+print("Original set elements:")
+print(set_x)
+print(set_y)
+print("/nIntersection of two said sets:")
+set_z= set_x.intersection(set_y)
+print(set_z)
